16c2769c3cf522ef76898a89dd15dae48ccb6531
yjs2952 <yjs2952@naver.com>
2020-06-09 23:47:27 +0900

    201 응답받기

---
+++ src/main/java/com/study/demoinflearnrestapi/event/EventController.java
@@ -0,0 +1,23 @@
+package com.study.demoinflearnrestapi.event;
+
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@Controller
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
+public class EventController {
+
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody Event event){
+        URI createdUri = linkTo(EventController.class).slash("{id}").toUri();
+        return ResponseEntity.created(createdUri).body(event);
+    }
+}
--- src/test/java/com/study/demoinflearnrestapi/event/EventControllerTest.java
+++ src/test/java/com/study/demoinflearnrestapi/event/EventControllerTest.java
@@ -1,5 +1,6 @@
 package com.study.demoinflearnrestapi.event;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -7,7 +8,11 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest // MockMvc 빈을 자동 설정 해준다. 웹 관련 빈만 등록해 준다.
@@ -17,12 +22,31 @@ public class EventControllerTest {
     MockMvc mockMvc;    // 스프링 MVC 테스트 핵심 클래스
                         // 웹서버를 띄우지 않고 스프링 MVC (DispatcherServlet)가 요청을 처리하는 과정을 확인할 수 있기 때문에 컨트롤러 테스트용으로 자주 쓰임
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 23, 14, 21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018, 11, 24, 14, 21))
+                .beginEventTime(LocalDateTime.of(2018, 11, 25, 14, 21))
+                .endEventTime(LocalDateTime.of(2018, 11, 26, 14, 1))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
+
         mockMvc.perform(post("/api/events")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaTypes.HAL_JSON)
-        )
-                .andExpect(status().isCreated());
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaTypes.HAL_JSON_VALUE)
+                    .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+        ;
     }
 }
