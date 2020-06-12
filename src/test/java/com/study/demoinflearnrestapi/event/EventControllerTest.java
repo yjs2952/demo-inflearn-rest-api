fb25ae0e447259131e83d07be31751262880374d
yjs2952 <yjs2952@naver.com>
2020-06-12 12:15:05 +0900

    add test description annotation

--- src/test/java/com/study/demoinflearnrestapi/event/EventControllerTest.java
+++ src/test/java/com/study/demoinflearnrestapi/event/EventControllerTest.java
@@ -1,6 +1,7 @@
 package com.study.demoinflearnrestapi.event;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.demoinflearnrestapi.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +37,7 @@ public class EventControllerTest {
 //    EventRepository eventRepository;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     void createEvent() throws Exception {
         EventDto event = EventDto.builder()
                 .name("Spring")
@@ -69,6 +71,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     void createEvent_bad_request() throws Exception {
         Event event = Event.builder()
                 .id(100)
@@ -100,6 +103,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
     void createEvent_bad_request_empty_input() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
@@ -121,6 +125,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     void createEvent_bad_request_wrong_input() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .build();
